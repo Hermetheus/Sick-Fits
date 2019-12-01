c258025b36c833665ebe98b1b00b5e6fc6a6a205
Hermetheus <allanmolson@gmail.com>
2019-12-01 12:30:02 -0600

    Hook up Appollo to Front End

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "11ea3e641f5c17302dd5";
+/******/ 	var hotCurrentHash = "bf2124a2bfb955d218ea";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
